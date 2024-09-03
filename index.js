deacdb0410216685743dfc8cc81d6852240cc0ec
Ishan Dhotre <125141868+ishan-26@users.noreply.github.com>
2024-09-04 01:27:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,11 +25,3 @@ function sendMail() {
         alert("Error sending message")
         })
 }
-
-function clear() {
-  document.getElementById("fname").value = "";
-  document.getElementById("femail").value = "";
-  document.getElementById("fcontact").value = "";
-  document.getElementById("fmessage").value = "";
-}
-
