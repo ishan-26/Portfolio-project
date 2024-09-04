c888139f5d20118fde2e81c64508ac795eed10b8
Ishan Dhotre <125141868+ishan-26@users.noreply.github.com>
2024-09-04 11:19:13 +0530

    Delete clearButton.js

--- clearButton.js
+++
@@ -1,6 +0,0 @@
-function clear() {
-   document.getElementById("fname").value = ""  // clears those fields
-  document.getElementById("femail").value = ""
-  document.getElementById("fcontact").value = ""
-  document.getElementById("fmessage").value = ""
-}
